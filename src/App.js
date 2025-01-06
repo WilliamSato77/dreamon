460ebf218f9ab9a900b464d3ec0d2916e7ca409f
WilliamSato77 <ericsato77@gmail.com>
2025-01-06 14:57:29 +0200

    styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/navigation/Nav.js';
 import Footer from './components/footer/Footer.js';
